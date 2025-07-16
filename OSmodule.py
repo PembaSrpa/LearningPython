4c02a19c9a4238321dcb0e51660bbcb1f2ca7279
Nirashanichang <nirashanichang@gmail.com>
2025-07-16 17:06:26 +0545

    os2

--- OSmodule.py
+++ OSmodule.py
@@ -24,3 +24,22 @@ pprint(os.stat('0cooltxt.py'))
 pprint(os.stat('0cooltxt.py').st_size)
 mod_time = os.stat('0cooltxt.py').st_mtime
 print(datetime.fromtimestamp(mod_time)) #human readable time
+
+for dirpath, dirnames, filenames in os.walk('/home/nirashanichang/Desktop/Python Projects/'):
+    pprint(('Current Path:', dirpath))
+    pprint(('Directories:', dirnames))
+    pprint(('Files:', filenames))
+    pprint('')
+
+q = os.environ.get('HOME')
+pprint(q)
+
+file_path = os.path.join(os.environ.get('HOME'), 'test.txt')
+pprint(file_path)
+
+pprint(os.path.basename('tmp/test.txt'))
+pprint(os.path.split('tmp/test.txt'))
+pprint(os.path.exists('tmp/test.txt'))
+pprint(os.path.isdir('tmp/test.txt'))
+pprint(os.path.isfile('tmp/test.txt'))
+pprint(os.path.splitext('tmp/test.txt'))
