c5f59091f0cbc0a73916de2c516b97354db7ae3e
Nirashanichang <nirashanichang@gmail.com>
2025-07-19 16:27:40 +0545

    unittest

--- trycatch.py
+++ trycatch.py
@@ -1,1 +1,17 @@
-f = open('testfile.txt')
+try:
+    f = open('testfile.txt')
+    if f.name == 'currupt.txt':
+        raise Exception #custom exception
+except FileNotFoundError:
+    print('File Doesnt exist')
+except Exception as e:
+    print("An error occurred:", e)
+else: #runs if theres no exception
+    print("File opened successfully")
+    # You can add code here to work with the file
+    f.close()
+finally: #runs anyway
+    print("Execution completed, whether an error occurred or not.")
+    if 'f' in locals() and not f.closed:
+        f.close()
+        print("File closed.")
